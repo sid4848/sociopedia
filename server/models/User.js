7d7c476ba9b6432dd277c0fedc4e6c8b2cd2a846
sid4848 <shitalsiddhagavali4848@gmail.com>
2023-05-20 10:13:17 +0530

    commit

--- server/models/User.js
+++ server/models/User.js
@@ -41,5 +41,5 @@ const userSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("Mediauser", userSchema);
 export default User;
